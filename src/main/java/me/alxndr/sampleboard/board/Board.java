311955e33da25e36799aa9bee6d61ea5d7f72dbc
Alexander Choi <dev.alxndr@gmail.com>
2021-08-03 11:21:24 +0900

    board entity init

--- src/main/java/me/alxndr/sampleboard/board/Board.java
+++ src/main/java/me/alxndr/sampleboard/board/Board.java
@@ -2,9 +2,12 @@ package me.alxndr.sampleboard.board;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Builder.Default;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import me.alxndr.sampleboard.common.YnType;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.data.annotation.CreatedDate;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
@@ -28,12 +31,21 @@ public class Board {
 
     private String content;
 
+    @Default
+    private int views = 0;
+
+    private YnType pinedYn;
+
+    @Enumerated(EnumType.STRING)
     private YnType isDeleted;
 
+    @Enumerated(EnumType.STRING)
     private YnType isPublished;
 
-    private LocalDateTime
+    @CreatedDate
+    private LocalDateTime createdAt;
 
-
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
 
 }
